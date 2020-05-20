ddce1351424d5d0683ae97c3e56bae154219f042
Ed Ignot <edignot@gmail.com>
2020-05-20 17:33:24 -0600

    Updates

---
+++ precache-manifest.8965e2bd0666a66c0e953bcde76e9544.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a30e381667f9e9d6039a761aa91df27",
+    "url": "/vrad/index.html"
+  },
+  {
+    "revision": "e6c2a606476959164691",
+    "url": "/vrad/static/css/main.58cdcf43.chunk.css"
+  },
+  {
+    "revision": "2423b8928d653aa927b0",
+    "url": "/vrad/static/js/2.a322be02.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vrad/static/js/2.a322be02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6c2a606476959164691",
+    "url": "/vrad/static/js/main.d01b6f16.chunk.js"
+  },
+  {
+    "revision": "b173dfadf4d37f4862fe",
+    "url": "/vrad/static/js/runtime-main.bf87012a.js"
+  }
+]);
